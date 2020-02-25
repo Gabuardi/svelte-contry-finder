dafd852f3ccd21a4f0d89c466c0b421c848b16ea
Josué Gabuardi <jgabuardi@beenverified.com>
2020-02-25 01:56:49 -0600

    fix catch handler

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,23 @@
 	let result;
 
 	async function getResult() {
-		let response = await fetch(`https://restcountries.eu/rest/v2/name/${search}`);
-		const data = await response.json();
-		console.log(data);
-		result = data;
+		const response = await fetch(`https://restcountries.eu/rest/v2/name/${search}`);
+		const responseFormatted = await response.json();
+
+		if (response.ok) {
+			return responseFormatted;
+		} else {
+			const text = await response.text();
+			throw new Error(text);
+		}
+
+		// const data = await response.json();
+		// console.log(data);
+		// result = data;
+	}
+
+	function buttonHandler() {
+		result = getResult();
 	}
 </script>
 
@@ -20,18 +33,18 @@
 	<input bind:value={search}>
 </label>
 
-<button on:click={getResult}>Search</button>
+<button on:click={buttonHandler}>Search</button>
 
-{#if result!==undefined}
 
 	{#await result}
 		<p>Loading...</p>
 	{:then value}
-		<p>name: {value.name}</p>
-		<p>capital: {value.capital}</p>
-		<p>population: {value.population}</p>
+		{#if value}
+			<p>name: {value.name}</p>
+			<p>capital: {value.capital}</p>
+			<p>population: {value.population}</p>
+		{/if}
 	{:catch error}
-		<p>{error}</p>
+		<p>AAAAAA</p>
 	{/await}
 
-{/if}
