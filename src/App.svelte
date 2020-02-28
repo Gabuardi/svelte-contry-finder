a2f35f120fd0971b8bd5227cc01993ef06307d93
Josué Gabuardi <jgabuardi@beenverified.com>
2020-02-28 16:54:40 -0600

    add style for input and button

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,44 @@
+<!----------- LOGIC ---------->
 <script>
 	import Searcher from './Searcher.svelte';
 
 	let data;
 </script>
 
+<!----------- STYLE ---------->
+<style>
+	:global(body) {
+		padding-top: 50px;
+		background-color: #404040;
+	}
 
-<Searcher bind:result={data}/>
+	.searcher {
+		width: 90%;
+		margin: auto;
+	}
 
-	{#await data}
-		<p>Loading...</p>
-	{:then value}
-		{#if value}
-			<p>name: {value.name}</p>
-			<p>capital: {value.capital}</p>
-			<p>population: {value.population}</p>
-		{/if}
-	{:catch error}
-		<p>{error}</p>
-	{/await}
+	@media (min-width: 765px) {
+		.searcher {
+			width: 80%;
+		}
+	}
+</style>
+
+<!----------- TEMPLATE ---------->
+<div class="searcher">
+	<Searcher bind:result={data}/>
+</div>
+
+
+{#await data}
+	<p>Loading...</p>
+{:then value}
+	{#if value}
+		<p>name: {value.name}</p>
+		<p>capital: {value.capital}</p>
+		<p>population: {value.population}</p>
+	{/if}
+{:catch error}
+	<p>{error}</p>
+{/await}
 
--- src/Searcher.svelte
+++ src/Searcher.svelte
@@ -21,14 +21,57 @@
 
 <!----------- STYLE ---------->
 <style>
+    .container {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+
+    input, button {
+        width: 100%;
+        height: 50px;
+        border: none;
+        outline: none;
+        transition: all 0.3s;
+        text-transform: uppercase;
+    }
+
     input {
-        background-color: yellow;
+        padding: 10px;
+    }
+
+    button {
+        font-size: 20px;
+        background-color: orange;
+    }
+
+    input:focus {
+        border: solid orange 2px;
+    }
+
+    button:hover {
+        background-color: #c47200;
+    }
+
+    @media (min-width: 765px) {
+        .container {
+            flex-direction: row;
+        }
+        label {
+            width: 100%;
+            margin-right: 20px;
+        }
+        button {
+            width: 20%;
+        }
     }
 </style>
 
 <!----------- TEMPLATE ---------->
+<div class="container">
 <label>
-    <input bind:value={search}>
+    <input placeholder="Search a Country" bind:value={search}>
 </label>
 
 <button on:click={buttonHandler}>Search</button>
+</div>
